3d35d5cf9cd5623796a92a9119e863e916fa1f43
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-09-18 21:02:11 -0500

    Clase usuario para guardar los datos del usuario y gestionar el formateo de los datos

--- app/src/main/java/com/example/asistencia/Usuario.kt
+++ app/src/main/java/com/example/asistencia/Usuario.kt
@@ -1,9 +1,7 @@
 package com.example.asistencia
 
-import java.io.Serializable
-
-data class Usuario(val nombres: String, val apellidos: String, val correo: String, val telefono: String, val grupoSanguineo: String) : Serializable {
+data class Usuario(val nombres: String, val apellidos: String, val correo: String, val telefono: String, val grupoSanguineo: String) {
     override fun toString(): String {
-        return "Nombres: ${nombres}\nApellidos: ${apellidos}\nCorreo: ${correo}\nTelefono: ${telefono}\nGrupo Sanguineo: ${grupoSanguineo}\n"
+        return "\nNombres: ${nombres}\nApellidos: ${apellidos}\nCorreo: ${correo}\nTelefono: ${telefono}\nGrupo Sanguineo: ${grupoSanguineo}"
     }
 }
